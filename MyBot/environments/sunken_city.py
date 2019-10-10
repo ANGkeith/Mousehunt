634e516f004a4215a2dd1e7987f85c3e20a1e0de
ANGkeith <kang024@e.ntu.edu.sg>
2019-10-10 10:31:01 +0800

    fix sunken city script

--- MyBot/environments/sunken_city.py
+++ MyBot/environments/sunken_city.py
@@ -1,8 +1,16 @@
-from MyBot.bot import Bot
-from MyBot.hud import disArmCharm, armCharm
+# Standard Library
 from typing import Any
 
-ANCHOR_ZONE = ["Sunken Treasure", "Pearl Patch", "Sand Dollar Sea Bar", "Oxygen Stream", "Deep Oxygen Strem"]
+from MyBot.bot import Bot
+from MyBot.hud import armCharm, disArmCharm
+
+ANCHOR_ZONE = [
+    "Sunken Treasure",
+    "Pearl Patch",
+    "Sand Dollar Sea Bar",
+    "Oxygen Stream",
+    "Deep Oxygen Strem",
+]
 
 
 def prepare(bot: Bot) -> None:
@@ -15,4 +23,9 @@ def prepare(bot: Bot) -> None:
 
 
 def is_anchor_zone(bot: Bot) -> bool:
-    return bot.driver.find_elements_by_class_name("zoneName") in ANCHOR_ZONE
+    return (
+        bot.driver.find_elements_by_class_name("zoneName")[0].get_attribute(
+            "innerText"
+        )
+        in ANCHOR_ZONE
+    )
