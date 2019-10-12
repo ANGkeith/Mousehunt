27873b6b7604d603cfb2d6143637d546bfe15082
ANGkeith <kang024@e.ntu.edu.sg>
2019-10-13 01:15:40 +0800

    script for travelling

---
+++ MyBot/travel.py
@@ -0,0 +1,55 @@
+# Standard Library
+from time import sleep
+
+from MyBot.bot import Bot
+from MyBot.hud import go_to_camp_page
+
+
+def travel(bot: Bot, region: str, environment: str) -> None:
+    """
+    :params region: name of the region
+    :params environments: name of the environment
+    """
+    go_to_travel_page(bot)
+    sleep(1)
+    select_region(bot, region)
+    sleep(1)
+    select_environment(bot, environment)
+    sleep(1)
+    travel_to_environment(bot)
+    sleep(1)
+    go_to_camp_page(bot)
+
+
+def go_to_travel_page(bot: Bot) -> None:
+    bot.driver.find_element_by_xpath(
+        "//li[@class='travel']" "//a[@class='mousehuntHud-menu-item root']"
+    ).click()
+
+
+def select_region(bot: Bot, region: str) -> None:
+    regions = bot.driver.find_elements_by_xpath(
+        "//a[@class='travelPage-map-region-name']"
+    )
+    for r in regions:
+        if region == r.text:
+            r.click()
+
+
+def select_environment(bot: Bot, environment: str) -> None:
+    environments = bot.driver.find_elements_by_xpath(
+        "//div[@class='travelPage-map-region active']"
+        "//a[@class='travelPage-map-region-environment-link ']"
+    )
+    for e in environments:
+        if environment == e.text:
+            e.click()
+
+
+def travel_to_environment(bot: Bot) -> None:
+    element = bot.driver.find_element_by_xpath(
+        "//div[contains(@class,'travelPage-map-image-wrapper')]"
+        "//div[contains(@class,'highlight')]"
+        "//div[@class='travelPage-map-image-environment-button']"
+    )
+    element.click()
