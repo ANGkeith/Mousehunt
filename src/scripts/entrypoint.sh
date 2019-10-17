d7f8bff40d83d8d82f6099fbebfcd0e578b0ad31
ANGkeith <kang024@e.ntu.edu.sg>
2019-10-18 01:14:38 +0800

    check that the sound is working

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -11,5 +11,8 @@ for (( i=0; i<num_of_users; i++ )); do
     python3 start.py ${usernames[@]:$i:1} ${passwords[@]:$i:1} &
 done
 
+# sound_check
+play -nq -t alsa synth 5 sine 200
+
 # Use to prevent the script from exiting
 tail -f ./log/*.log
