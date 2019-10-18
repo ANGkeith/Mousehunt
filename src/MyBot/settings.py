3750c2dd71e92c379a30f8105453c8043d6eb597
ANGkeith <kang024@e.ntu.edu.sg>
2019-10-18 11:19:21 +0800

    incorporate main function practise

--- src/MyBot/settings.py
+++ src/MyBot/settings.py
@@ -5,4 +5,4 @@ import sys
 NORMAL_DELAY = 75
 NIGHT_TIME_DELAY = 3000
 URL = "https://www.mousehuntgame.com"
-
+LOG_FILE_PATH = f"log/{sys.argv[1]}.log"
--- src/start.py
+++ src/start.py
@@ -1,47 +1,51 @@
 # Standard Library
-import sys
 import logging
 from time import sleep
 
 from MyBot.bot import Bot
 from MyBot.utils import log_identifier, noise_generator
+from MyBot.settings import LOG_FILE_PATH
 from selenium.common.exceptions import WebDriverException
 
-LOG_DIR = "log/"
-LOG_FILE = f"{sys.argv[1]}.log"
-logging.basicConfig(filename=LOG_DIR + LOG_FILE, level=logging.INFO)
 
-try:
-    myBot = Bot()
-except WebDriverException:
-    noise = noise_generator()
-    logging.error(
-        f"{log_identifier()} Browser has crashed, going to "
-        f"relaunch the browser for one more time in {noise} seconds"
-    )
-    sleep(noise)
-    myBot = Bot()
+def main() -> None:
+    logging.basicConfig(filename=LOG_FILE_PATH, level=logging.INFO)
 
-number_of_retries = 0
-
-
-while True:
     try:
-        myBot.start()
+        myBot = Bot()
     except WebDriverException:
-        if number_of_retries < 3:
-            number_of_retries += 1
-            logging.error(
-                f"{log_identifier()} Browser has crashed, attempting to "
-                f"relaunch the browser. (Retries left: {3 - number_of_retries})"
-            )
-            myBot = Bot()
-        else:
-            logging.error(
-                f"{log_identifier()} Browser has crashed too many times. "
-                "Goodbye"
-            )
+        noise = noise_generator()
+        logging.error(
+            f"{log_identifier()} Browser has crashed, going to "
+            f"relaunch the browser for one more time in {noise} seconds"
+        )
+        sleep(noise)
+        myBot = Bot()
+
+    number_of_retries = 0
+
+    while True:
+        try:
+            myBot.start()
+        except WebDriverException:
+            if number_of_retries < 3:
+                number_of_retries += 1
+                logging.error(
+                    f"{log_identifier()} Browser has crashed, attempting to "
+                    f"relaunch the browser. (Retries left: "
+                    "{3 - number_of_retries})"
+                )
+                myBot = Bot()
+            else:
+                logging.error(
+                    f"{log_identifier()} Browser has crashed too many times. "
+                    "Goodbye"
+                )
+                break
+        except Exception as e:
+            logging.exception(e)
             break
-    except Exception as e:
-        logging.error(e)
-        break
+
+
+if __name__ == "__main__":
+    main()
