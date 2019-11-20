cfdb2226313bd8d2cea139e586cb79d348551b8c
ANGkeith <kang024@e.ntu.edu.sg>
2019-11-20 18:41:26 +0800

    readjust some time settings

--- src/MyBot/utils.py
+++ src/MyBot/utils.py
@@ -22,11 +22,15 @@ def to_lower_case_with_underscore(string: str) -> str:
 
 
 def is_sleeping_time() -> bool:
-    return int(time.strftime("%H")) < 7
+    return int(time.strftime("%H")) < 5
+
+
+def no_sound_time() -> bool:
+    return int(time.strftime("%H")) < 10
 
 
 def play_sound() -> None:
-    if not is_sleeping_time():
+    if not no_sound_time():
         duration = 1  # seconds
         freq = 200  # Hz
         os.system("play -nq -t alsa synth {} sine {}&".format(duration, freq))
