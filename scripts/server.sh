f0f5ca1f32fc5ebb0c4e303d1387a74c37c5611f
ANGkeith <kang024@e.ntu.edu.sg>
2020-02-25 02:41:08 +0800

    netcat bsd instead of gnu

--- scripts/server.sh
+++ scripts/server.sh
@@ -2,4 +2,4 @@
 script_directory=$(cd $(dirname ${BASH_SOURCE}) && pwd -P)
 project_root=$(cd $(dirname ${script_directory}) && pwd -P)
 
-while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; sh "${project_root}/scripts/status.sh"; } | nc -l -c -p 54321; done
+while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; . "${project_root}/scripts/status.sh"; } | nc -l -N -p 54321; done
